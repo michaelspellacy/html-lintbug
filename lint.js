106dc8d34248f07a03c0a242abc4fc063693d0f6
michaelspellacy <spell@spellacy.net>
2017-03-09 18:13:50 -0500

    Update

---
+++ lint.js
@@ -0,0 +1,19 @@
+
+// QA Worm 0.3
+// Developer: Michael "Spell" Spellacy, Developer: Michael "Spell" Spellacy. Twitter: @spellacy, GitHub: michaelspellacy
+// Dependency: jQuery (Plain Vanilla coming soon)
+
+$(function() {
+
+  $('head').append('<link rel="stylesheet" href="https://michaelspellacy.github.io/qa-worm/lint.css">');
+
+  $('html').addClass("lint").append('<button id="btn-lint" title="HTML Linter">QA Worm</button>');
+
+  $('#btn-lint').on('click', function(){
+
+    $(this).toggleClass('active');
+    $('html').toggleClass('lint');
+
+  });
+
+});
