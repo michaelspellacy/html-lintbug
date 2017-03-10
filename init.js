2b7d2e9aa908d2260d756e3a0cc0ad965df667b5
michaelspellacy <spell@spellacy.net>
2017-03-10 18:27:14 -0500

    Name change

--- init.js
+++ init.js
@@ -1,19 +1,25 @@
-
-// QA Worm 0.3
+// HTML LintBug 0.5
 // Developer: Michael "Spell" Spellacy, Developer: Michael "Spell" Spellacy. Twitter: @spellacy, GitHub: michaelspellacy
-// Dependency: jQuery (Plain Vanilla coming soon)
+// Dependency: jQuery
 
 $(function() {
 
-  $('head').append('<link rel="stylesheet" href="https://michaelspellacy.github.io/qa-worm/lint.css"/>');
+  $('html').addClass("lintbug").append('<button id="btn-lintbug" type="button" title="HTML LintBug">Debug</button>');
 
-  $('html').addClass("lint").append('<button id="btn-lint" title="HTML Linter">QA Worm</button>');
+  // $('head').append('<link rel="stylesheet" href="lintbug.css">');
 
-  $('#btn-lint').on('click', function(){
+  $('head').append('<link id="html-lintbug" rel="stylesheet" href="https://michaelspellacy.github.io/html-lintbug/lintbug.css">');
+
+  $('#btn-lintbug').on('click', function(){
 
     $(this).toggleClass('active');
-    $('html').toggleClass('lint');
+    $('html').toggleClass('lintbug');
 
   });
+
+  // Rules for elements where pseudo's not possible
+
+  $('iframe:not([title])').after('<div class="lintbug-iframe"/>');
+  $('img:not([alt])').after('<div class="lintbug-img"/>');
 
 });
