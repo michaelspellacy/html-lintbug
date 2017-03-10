7b58a9881514bb7ea8fe6532dd5b967dd7b58d08
michaelspellacy <spell@spellacy.net>
2017-03-10 15:41:25 -0500

    Broken CSS

--- lint.js
+++ lint.js
@@ -7,9 +7,9 @@ $(function() {
 
   $('html').addClass("lint").append('<button id="btn-lint" type="button" title="HTML LintBug">Debug</button>');
 
-  $('head').append('<link rel="stylesheet" href="lint.css">');
+  // $('head').append('<link rel="stylesheet" href="lint.css">');
 
-  //$('head').append('<link rel="stylesheet" href="https://michaelspellacy.github.io/qa-worm/lint.css">');
+  $('head').append('<link rel="stylesheet" href="https://michaelspellacy.github.io/qa-worm/lint.css">');
 
   $('#btn-lint').on('click', function(){
 
