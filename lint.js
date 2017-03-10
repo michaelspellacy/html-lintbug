184333302cdea93384a6334cb0c22d487c450c48
michaelspellacy <spell@spellacy.net>
2017-03-10 15:09:17 -0500

    Misc

--- lint.js
+++ lint.js
@@ -5,7 +5,7 @@
 
 $(function() {
 
-  $('html').addClass("lint").append('<button id="btn-lint" title="HTML LintBug">QA Worm</button>');
+  $('html').addClass("lint").append('<button id="btn-lint" type="button" title="HTML LintBug">Debug</button>');
 
   // $('head').append('<link rel="stylesheet" href="lint.css">');
 
